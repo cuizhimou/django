1b0a18bcea470d712ae1bca663fce61eb4102478
崔智谋 <cui@cuideMacBook-Air.local>
2018-04-15 13:48:58 +0800

    user module

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -4,7 +4,7 @@ class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
     uemail = models.CharField(max_length=30)
-    ushow = models.CharField(max_length=20,default='')
+    ushou = models.CharField(max_length=20,default='')
     uaddress = models.CharField(max_length=100,default='')
     uyoubian = models.CharField(max_length=6,default='')
     uphone = models.CharField(max_length=11,default='')
